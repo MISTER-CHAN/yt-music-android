1768754fb50fa1d8ffa859e4921b9df34ab5453e
Chan Tsz-to <MISTER_CHAN@outlook.com>
2021-09-03 19:54:33 +0800

    Initial commit

--- app/src/main/java/com/mister_chan/ytmusic/MainActivity.java
+++ app/src/main/java/com/mister_chan/ytmusic/MainActivity.java
@@ -63,16 +63,19 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     player.loadUrl("javascript:" +
+                            "var player = " + PLAYER + ";");
+                    player.loadUrl("javascript:" +
                             "(function () {" +
                             "    var $cross = document.getElementsByClassName(\"ytp-ad-overlay-close-container\")[0];" +
                             "    var $skip = document.getElementsByClassName(\"ytp-ad-skip-button\")[0];" +
-                            "    if ($cross != undefined)" +
+                            "    if ($cross != undefined) {" +
                             "        $cross.click();" +
-                            "    if ($skip != undefined)" +
-                            "        $skip.click()" +
+                            "    }" +
+                            "    if ($skip != undefined) {" +
+                            "        $skip.click();" +
+                            "    }" +
                             "})()");
                     player.evaluateJavascript("" +
-                                    "var player = " + PLAYER + ";" +
                                     "if (player != null)" +
                                     "    player.getCurrentTime()",
                             new ValueCallback<String>() {
